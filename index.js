0e46603a6b7fbf1a98f0e02c4b8bc8ee238481cc
Madhav Rathi <mdhvrthi@gmail.com>
2017-12-10 21:23:57 +0530

    basic model

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const app = express();
+
+app.get('/', (req, res) => {
+  // do something here
+  res.send('TWITTER CHROME EXTENSION');
+})
+
+mongoose.connect(keys.mongoURI);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
